10a41eb537f98d82dbea4c20c6c4664df372f173
jdranpariya <jdpatel.code@gmail.com>
2025-01-20 17:42:25 +0100

    add plot to piont_transmitter

--- point_transmitter.py
+++ point_transmitter.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.constants import Avogadro 
 from config import NanomachineConfig
 from utils import generate_switching_pattern, get_conc_vol_for_practical, save_to_csv, plot_data
@@ -10,8 +11,8 @@ conf = NanomachineConfig()
 vol_in, vol_out, conc_in, conc_out = get_conc_vol_for_practical(conf.r_tx, conf.r_out)
 
 # Set Switching pattern
-switching_pattern = [1,0,0,0,1,1,1]
-Ts = 1
+switching_pattern = [1,0,1,0,1,0,1]
+Ts = 2
 conf.simulation_end = len(switching_pattern) * Ts
 
 # Time Array
@@ -39,7 +40,6 @@ def point_transmitter(time_array, switching_pattern, N, config):
     release_pattern = np.zeros_like(time_array)
     
     segment_length = len(time_array) // len(switching_pattern) 
-    print(f'segment length is {segment_length}')
 
     release_indices = []
     for i, state in enumerate(switching_pattern):
@@ -48,14 +48,13 @@ def point_transmitter(time_array, switching_pattern, N, config):
             release_indices.append(start_index)
 
     release_pattern[release_indices] = N
-    print(f'release indices is {release_indices}')
     
     # Create receiver and perform convolution to simulate reception of released molecules
     rec = AbsorbingReceiver(config.r_rx) if config.receiver_type == 'AbsorbingReceiver' else TransparentReceiver(config.r_rx)
     
     # Simulate average hits at the receiver
-    Nrec = rec.average_hits(time_array, release_pattern, config.r_tx, config.D_space, config.dist)
-    Nrec *= config.step_time
+    nr = rec.average_hits(time_array, release_pattern, config.r_tx, config.D_space, config.dist)
+    Nrec = nr[:len(time_array)] * config.step_time
 
     # Convert release pattern to number of molecules outside
     NoutB = release_pattern * config.D_space * Avogadro * config.r_tx * config.dist
@@ -68,9 +67,25 @@ def point_transmitter(time_array, switching_pattern, N, config):
             }
 
 # Assuming time_array, Ts, and N are defined along with a Configuration object, config
-results = point_transmitter(time_array, switching_pattern, 300, conf)
+results = point_transmitter(time_array, switching_pattern, 100, conf)
 NoutB = results['NoutB']
 Nrec = results['Nrec']
 
-print("Number of B molecules released over time:", NoutB)
-print("Number of B molecules received over time:", Nrec)
+fig, ax1 = plt.subplots()
+
+# Plot number of B molecules released
+ax1.plot(time_array, NoutB, 'r', label='NoutB (Released)')
+ax1.set_xlabel('Time')
+ax1.set_ylabel('# B Molecules Released', color='r')
+ax1.tick_params(axis='y', labelcolor='r')
+ax1.set_ylim(0, max(NoutB) + 2)  # Adjust for better viewing
+
+# Instantiate a second y-axis that shares the same x-axis
+ax2 = ax1.twinx()
+ax2.plot(time_array, Nrec, 'k', label='Nrec (Received)')
+ax2.set_ylabel('# B Molecules Received', color='k')
+ax2.tick_params(axis='y', labelcolor='k')
+
+plt.title('Number of B Molecules Released and Received Over Time')
+plt.tight_layout()  # To prevent label overlap
+plt.show()
