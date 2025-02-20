fix hit prob calculation for point tx

archive/test_point_tx.py
@@ -11,9 +11,9 @@ conf = NanomachineConfig()
 vol_in, vol_out, conc_in, conc_out = get_conc_vol_for_practical(conf.r_tx, conf.r_out)
 
 # Set Switching pattern
-switching_pattern = [1,0,0,1,0,1,0,0,0,0,0]
+switching_pattern = [1,0,1,0,0,1,1,1,0,0,0]
 
-Ts = 10
+Ts = 300
 conf.simulation_end = len(switching_pattern) * Ts
 
 # Config Change
@@ -62,7 +62,7 @@ def point_transmitter(time_array, switching_pattern, config):
 
     # Create receiver and perform convolution to simulate reception of released molecules
     rec = AbsorbingReceiver(config.r_rx) if config.receiver_type == 'AbsorbingReceiver' else TransparentReceiver(config.r_rx)
-    #rec = TransparentReceiver(config.r_rx)
+    # rec = TransparentReceiver(config.r_rx)
 
     # Simulate average hits at the receiver
     # TODO change hitting probability https://ieeexplore.ieee.org/document/8742793
@@ -90,11 +90,11 @@ def point_transmitter(time_array, switching_pattern, config):
 
     print(f"Sum of Nrec_inst with avg_hits: {np.sum(Nrec_inst)}")
 
-    Nrec = Nrec_inst[:len(time_array)]  * config.step_time
+    Nrec = Nrec_inst[:len(time_array)]  # * config.step_time
 
     print(f"Sum of Nrec_inst after *config.step_time: {np.sum(Nrec)}")
 
-    Nrecv = np.cumsum(Nrec)
+    # Nrecv = np.cumsum(Nrec)
 
         # Plotting the results
     plt.figure(figsize=(10, 6))
@@ -102,7 +102,7 @@ def point_transmitter(time_array, switching_pattern, config):
     plt.plot(time_array, NoutB_instant, 'g--', label='NoutB Instantaneous Release')
     plt.plot(time_array, Nrec_inst[:len(time_array)], 'm:', label='Nrec Instantaneous')
     plt.plot(time_array, Nrec, 'b-', label='Nrec (Received)')
-    plt.plot(time_array, Nrecv, 'c-', label='Cumulative Received (Nrecv)')
+    # plt.plot(time_array, Nrecv, 'c-', label='Cumulative Received (Nrecv)')
 
     plt.xlabel('Time (s)')
     plt.ylabel('Molecules')
@@ -117,7 +117,7 @@ def point_transmitter(time_array, switching_pattern, config):
             'Nrec': Nrec,
             'NoutB_instant': NoutB_instant,
             'Nrec_inst': Nrec_inst,
-            'Nrecv': Nrecv
+            #'Nrecv': Nrecv
             }
 
 results = point_transmitter(time_array, switching_pattern, conf)

main.py
@@ -11,7 +11,7 @@ conf = NanomachineConfig()
 vol_in, vol_out, conc_in, conc_out = get_conc_vol_for_practical(conf.r_tx, conf.r_out)
 
 # Set Switching pattern
-switching_pattern = [1,0,1,0,1,0,0,0,0,0,0]
+switching_pattern = [1,1,0]
 
 random_bits = np.random.randint(0, 2, size=5)  # Generates 10 random bits (0 or 1)
 padding = np.zeros(3, dtype=int)  # Create an array of 5 zeros
@@ -20,7 +20,7 @@ switching_pattern = switching_pattern.tolist()
 
 print(switching_pattern)
 
-Ts = 50
+Ts = 80
 conf.simulation_end = len(switching_pattern) * Ts
 
 # Config Change
@@ -36,7 +36,9 @@ time_array = np.linspace(0,
 rho = generate_dyn_switching_pattern(switching_pattern,
                                  time_interval=Ts,
                                  length=len(time_array),
-                                 config=conf)
+                                 config=conf,
+                                 peak_duration_ratio=0.1,
+                                 zero_duration_ratio=0.1)
 
 # IdealTx
 results_ideal = ideal_transmitter(rho_array=rho.copy(),

models/space.py
@@ -98,13 +98,15 @@ class AbsorbingReceiver():
         arx -> reciever radius config.r_rx
         d0 -> dist in config
         '''
+        # dist = 6*dist
         beta1 = self.r_rx/dist
         beta2 = dist - self.r_rx
         beta3 = np.sqrt(4 * D * t)
         beta3 = np.divide(beta2, beta3, out=np.zeros_like(t), where=t!=0)
-        steepness = 50
-        prob = beta1 * erf(beta3) * np.exp(-steepness * np.linspace(0, 1, len(t)))
-        # print(prob)
+        prob = beta1 * erfc(beta3)
+        prob[0] = 0
+        print(t, prob)
+        print(f'Probability point tx absorbing rx: {prob}')
 
         return prob
 

point_transmitter.py
@@ -67,7 +67,7 @@ def point_transmitter(time_array, switching_pattern, config):
 
     # print(f"Sum of Nrec_inst with avg_hits: {np.sum(Nrec_inst)}")
 
-    Nrec = Nrec_inst[:config.step_count] * config.step_time
+    Nrec = Nrec_inst[:config.step_count] # * config.step_time
 
     # print(f"Sum of Nrec_inst after *config.step_time: {np.sum(Nrec_inst)}")
 
@@ -75,5 +75,5 @@ def point_transmitter(time_array, switching_pattern, config):
 
     return {
             'NoutB': NoutB,
-            'Nrec': Nrecv
+            'Nrec': Nrec
             }